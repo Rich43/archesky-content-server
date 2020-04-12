0ed65974d1bbccaa7ed358102f83d2006d4c783a
Richie Ward <RichieS@GMail.com>
2020-04-12 13:18:27 +0100

    Add initial token validation code

--- src/main/kotlin/com/pynguins/content/service/TokenService.kt
+++ src/main/kotlin/com/pynguins/content/service/TokenService.kt
@@ -1,8 +1,23 @@
 package com.pynguins.content.service
 
-class TokenService {
-    fun validateToken(token: String): Any? {
+import com.auth0.jwk.JwkProvider
+import com.auth0.jwk.UrlJwkProvider
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import java.security.interfaces.RSAPublicKey
+import java.util.*
 
-        return null
+
+class TokenService {
+    fun validateToken(token: String): DecodedJWT {
+        val prop = Properties()
+        prop.load(javaClass.classLoader.getResourceAsStream("application.properties"))
+        val jwt = JWT.decode(token)
+        val provider: JwkProvider = UrlJwkProvider(prop.getProperty("openid.connect.certificate.url"))
+        val jwk = provider[jwt.keyId]
+        val algorithm: Algorithm = Algorithm.RSA256(jwk.publicKey as RSAPublicKey, null)
+        algorithm.verify(jwt)
+        return jwt
     }
 }
