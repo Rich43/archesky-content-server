0a33076a3dc795a5b5e54a51f9e3aa801bcd2bca
Richie Ward <RichieS@GMail.com>
2020-04-12 14:51:02 +0100

    Add working implementation of validateToken, check issuer and signature is valid.

--- src/main/kotlin/com/pynguins/content/service/TokenService.kt
+++ src/main/kotlin/com/pynguins/content/service/TokenService.kt
@@ -5,6 +5,8 @@ import com.auth0.jwk.UrlJwkProvider
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.interfaces.DecodedJWT
+import graphql.GraphQLException
+import java.net.URL
 import java.security.interfaces.RSAPublicKey
 import java.util.*
 
@@ -13,11 +15,19 @@ class TokenService {
     fun validateToken(token: String): DecodedJWT {
         val prop = Properties()
         prop.load(javaClass.classLoader.getResourceAsStream("application.properties"))
+        val certificateURL = prop.getProperty("openid.connect.certificate.url")
+        val issuer = prop.getProperty("openid.connect.issuer")
         val jwt = JWT.decode(token)
-        val provider: JwkProvider = UrlJwkProvider(prop.getProperty("openid.connect.certificate.url"))
+        val provider: JwkProvider = UrlJwkProvider(URL(certificateURL))
         val jwk = provider[jwt.keyId]
         val algorithm: Algorithm = Algorithm.RSA256(jwk.publicKey as RSAPublicKey, null)
         algorithm.verify(jwt)
+        if (jwt.issuer != issuer) {
+            throw GraphQLException(
+                    "Token validation failed: " +
+                            "Invalid issuer (Expected: '$issuer' but got '${jwt.issuer}')"
+            )
+        }
         return jwt
     }
 }
