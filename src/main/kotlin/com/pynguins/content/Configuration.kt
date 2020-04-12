d278d32c798d85f26e8536c5e2f4cc4876719994
Richie Ward <RichieS@GMail.com>
2020-04-12 12:56:36 +0100

    Use properties file to store database credentials, add beginnings of a token service

--- src/main/kotlin/com/pynguins/content/Configuration.kt
+++ src/main/kotlin/com/pynguins/content/Configuration.kt
@@ -4,21 +4,18 @@ import com.arangodb.ArangoDB.Builder
 import com.arangodb.springframework.annotation.EnableArangoRepositories
 import com.arangodb.springframework.config.ArangoConfiguration
 import org.springframework.context.annotation.Configuration
+import java.util.*
 
 @Configuration
 @EnableArangoRepositories(basePackages = ["com.pynguins.content"])
-class DemoConfiguration : ArangoConfiguration {
+class Configuration : ArangoConfiguration {
     override fun arango(): Builder {
         return Builder()
-                .host(
-                        "localhost",
-                        8529
-                )
-                .user("root")
-                .password("Password1")
     }
 
     override fun database(): String {
-        return "_system"
+        val prop = Properties()
+        prop.load(javaClass.classLoader.getResourceAsStream("arangodb.properties"))
+        return prop.getProperty("arangodb.database")
     }
 }
---
+++ src/main/kotlin/com/pynguins/content/service/TokenService.kt
@@ -0,0 +1,8 @@
+package com.pynguins.content.service
+
+class TokenService {
+    fun validateToken(token: String): Any? {
+
+        return null
+    }
+}
