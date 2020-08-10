fac742b01823a33bc4cee14c22ab4ee0fc255861
Richie Ward <RichieS@GMail.com>
2020-08-10 21:46:54 +0100

    dummy subscription function wip

--- src/main/kotlin/com/archesky/content/resolvers/Subscription.kt
+++ src/main/kotlin/com/archesky/content/resolvers/Subscription.kt
@@ -1,7 +1,11 @@
 package com.archesky.content.resolvers
 
+import com.archesky.content.dto.Content
 import graphql.kickstart.tools.GraphQLSubscriptionResolver
 import javax.jms.Queue
 
 class Subscription(private val queue: Queue): GraphQLSubscriptionResolver {
+    fun updateContent(): Content {
+        return Content("1", "foo")
+    }
 }
