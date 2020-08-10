db3df9366dfbbe5e7272822e5510c82cac2fa927
Richie Ward <RichieS@GMail.com>
2020-08-10 21:50:33 +0100

    Add webflux dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:7.1.0")
+	implementation("com.graphql-java-kickstart:graphql-kickstart-spring-webflux:7.1.0")
 	implementation("com.graphql-java-kickstart:graphql-kickstart-spring-boot-starter-tools:7.1.0")
 	implementation("com.arangodb:arangodb-java-driver:6.6.0")
 	implementation("com.arangodb:arangodb-spring-data:3.2.3")
