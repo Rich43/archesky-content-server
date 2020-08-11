f36e9144187f8686b20ccdf0589814e802adec69
Richie Ward <RichieS@GMail.com>
2020-08-11 18:54:50 +0100

    Add missing annotation

--- src/main/kotlin/com/archesky/content/resolvers/Subscription.kt
+++ src/main/kotlin/com/archesky/content/resolvers/Subscription.kt
@@ -5,7 +5,9 @@ import com.archesky.content.service.ContentQueueService
 import graphql.kickstart.tools.GraphQLSubscriptionResolver
 import org.reactivestreams.Publisher
 import org.springframework.messaging.Message
+import org.springframework.stereotype.Component
 
+@Component
 class Subscription(private val contentQueueService: ContentQueueService): GraphQLSubscriptionResolver {
     fun updateContent(): Publisher<Message<Content>> {
         return contentQueueService.jmsReactiveSource()
