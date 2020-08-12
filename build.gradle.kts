ebfd8f3ce7b02ef9826097c0a45a5e01a0c12671
Richie Ward <RichieS@GMail.com>
2020-08-12 21:34:51 +0100

    Remove unneeded dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,8 +41,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-activemq")
-	implementation("org.springframework.integration:spring-integration-core")
-	implementation("org.springframework.integration:spring-integration-jms")
 	implementation("com.google.code.gson:gson:2.8.6")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
