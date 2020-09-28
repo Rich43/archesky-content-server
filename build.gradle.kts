f31ee7e11bd061c4942f961c26fa4b858fbe0c21
Richie Ward <RichieS@GMail.com>
2020-09-28 21:44:04 +0100

    Bump auth library version

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 	implementation("com.graphql-java-kickstart:graphql-kickstart-spring-boot-starter-tools:7.1.0")
 	implementation("com.arangodb:arangodb-java-driver:6.6.0")
 	implementation("com.arangodb:arangodb-spring-data:3.2.3")
-	implementation("com.archesky.auth.library:archesky-auth-library:0.0.275439014-SNAPSHOT")
+	implementation("com.archesky.auth.library:archesky-auth-library:0.0.277358918-SNAPSHOT")
 	implementation("com.archesky.common.library:archesky-common-library:0.0.277322317-SNAPSHOT")
 	runtimeOnly("com.graphql-java-kickstart:altair-spring-boot-starter:7.1.0")
 	runtimeOnly("com.graphql-java-kickstart:graphiql-spring-boot-starter:7.1.0")
