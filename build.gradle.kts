ff230e9e10407f6664423884b12229dba8809fe6
Richie Ward <RichieS@GMail.com>
2020-10-01 21:10:39 +0100

    Bump ssl library

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
 	implementation("com.arangodb:arangodb-java-driver:6.6.0")
 	implementation("com.arangodb:arangodb-spring-data:3.2.3")
 	implementation("com.archesky.auth.library:archesky-auth-library:0.0.281083311-SNAPSHOT")
-	implementation("com.archesky.ssl.library:archesky-ssl-library:0.0.281034691-SNAPSHOT")
+	implementation("com.archesky.ssl.library:archesky-ssl-library:0.0.283041548-SNAPSHOT")
 	runtimeOnly("com.graphql-java-kickstart:altair-spring-boot-starter:7.1.0")
 	runtimeOnly("com.graphql-java-kickstart:graphiql-spring-boot-starter:7.1.0")
 	runtimeOnly("com.graphql-java-kickstart:voyager-spring-boot-starter:7.1.0")
