11deb28e99ad98cd9aa7b2d5b1c87479477c428b
Richie Ward <RichieS@GMail.com>
2021-01-01 21:14:03 +0000

    Fix issue with collection not being created on when create is called

--- src/main/kotlin/com/archesky/content/dto/Content.kt
+++ src/main/kotlin/com/archesky/content/dto/Content.kt
@@ -3,8 +3,7 @@ package com.archesky.content.dto
 import com.arangodb.springframework.annotation.Document
 import org.springframework.data.annotation.Id
 
-const val CONTENT = "content"
-@Document(CONTENT)
+@Document("content")
 data class Content (
     @Id var id: String? = null,
     var content: String? = null
--- src/main/kotlin/com/archesky/content/repository/ContentRepository.kt
+++ src/main/kotlin/com/archesky/content/repository/ContentRepository.kt
@@ -1,18 +1,17 @@
 package com.archesky.content.repository
 
 import com.arangodb.springframework.annotation.Query
-import com.arangodb.springframework.repository.ArangoRepository;
-import com.archesky.content.dto.CONTENT
+import com.arangodb.springframework.repository.ArangoRepository
 import com.archesky.content.dto.Content
 import org.springframework.data.repository.query.Param
 
 interface ContentRepository : ArangoRepository<Content, String> {
-    @Query(value = "INSERT { content: @content } INTO $CONTENT RETURN NEW")
+    @Query(value = "INSERT { content: @content } INTO #collection RETURN NEW")
     fun createContent(@Param("content") content: String): Content
 
     @Query(value =
-            "FOR c IN $CONTENT FILTER c._key == @key " +
-            "UPDATE { _key: c._key, content: @content } IN $CONTENT RETURN NEW"
+            "FOR c IN #collection FILTER c._key == @key " +
+            "UPDATE { _key: c._key, content: @content } IN #collection RETURN NEW"
     )
     fun updateContentById(@Param("key") id: String, @Param("content") content: String): Content
 
