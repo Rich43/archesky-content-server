e53dd0b3908b8f80a9ee887a1023547194fa366d
Richie Ward <RichieS@GMail.com>
2021-01-04 21:21:07 +0000

    Catch EmptyResultDataAccessException

--- src/main/kotlin/com/archesky/content/service/MutationService.kt
+++ src/main/kotlin/com/archesky/content/service/MutationService.kt
@@ -5,6 +5,10 @@ import com.archesky.content.dto.ContentRevision
 import com.archesky.content.repository.ContentRepository
 import com.archesky.content.repository.ContentRevisionRepository
 import graphql.GraphQLException
+import org.apache.logging.log4j.LogManager.getLogger
+import org.apache.logging.log4j.Logger
+import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -12,13 +16,15 @@ import java.util.*
 class MutationService(private val contentRepository: ContentRepository,
                       private val contentRevisionRepository: ContentRevisionRepository,
                       private val contentQueueService: ContentQueueService) {
-//    @PreAuthorize("hasAuthority('archesky.create_content')")
+    val log: Logger = getLogger(MutationService::class)
+
+    @PreAuthorize("hasAuthority('archesky.create_content')")
     fun createContent(name: String, displayName: String): Content {
         try {
             contentRepository.findByName(name)
             throw GraphQLException("Content already exists")
-        } catch (e: Exception) { // TODO change me
-            // Ignore
+        } catch (e: EmptyResultDataAccessException) {
+            log.debug("createContent: No content row found.", e)
         }
         return contentRepository.createContent(name, displayName, Date(), Date())
     }
