b6562d4da642d13fb3ffcb9bebf7fc995d9f719a
Richie Ward <RichieS@GMail.com>
2021-01-04 21:34:25 +0000

    Fix date scalar type

--- src/main/kotlin/com/archesky/content/configuration/Scalars.kt
+++ src/main/kotlin/com/archesky/content/configuration/Scalars.kt
@@ -1,12 +1,12 @@
 package com.archesky.content.configuration
 
+import graphql.language.StringValue
 import graphql.schema.Coercing
 import graphql.schema.CoercingParseValueException
 import graphql.schema.CoercingSerializeException
 import graphql.schema.GraphQLScalarType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.lang.String.valueOf
 import java.time.Instant
 import java.time.format.DateTimeFormatter.ISO_INSTANT
 import java.util.*
@@ -21,26 +21,24 @@ class Scalars {
             .coercing(object : Coercing<Any?, Any?> {
                 override fun serialize(dataFetcherResult: Any?): Any? {
                     try {
-                        val date = valueOf(dataFetcherResult)
-                        return Date.from(Instant.from(ISO_INSTANT.parse(date)))
+                        val date: Date = dataFetcherResult as Date
+                        return ISO_INSTANT.format(date.toInstant())
                     } catch (e: Exception) {
                         throw CoercingSerializeException("Unable to serialize $dataFetcherResult as a date", e)
                     }
                 }
 
                 override fun parseValue(input: Any?): Any? {
-                    if (input is Date) {
-                        val date: Date = input
-                        return ISO_INSTANT.format(date.toInstant())
+                    if (input is String) {
+                        return Date.from(Instant.from(ISO_INSTANT.parse(input)))
                     } else {
                         throw CoercingParseValueException("Unable to parse variable value $input as a date")
                     }
                 }
 
                 override fun parseLiteral(input: Any?): Any? {
-                    if (input is Date) {
-                        val date: Date = input
-                        return ISO_INSTANT.format(date.toInstant())
+                    if (input is StringValue) {
+                        return Date.from(Instant.from(ISO_INSTANT.parse(input.value)))
                     } else {
                         throw CoercingParseValueException("Unable to parse variable value $input as a date")
                     }
